245223a3073a8666d4a14061e7f5a1237572a032
CartridgNSTU <97868606+CartridgNSTU@users.noreply.github.com>
2022-02-14 12:33:50 +0700

    кредит

---
+++ script.js
@@ -0,0 +1,11 @@
+let S = 2000000 ;
+let p = 10 ; 
+let years = 5 ;
+//считаем дискриминант//
+let Pereplata = 2000000;
+if (years >0) {
+Pereplata = (Pereplata/100*10);
+years - 1;
+}
+
+console.log(`Pereplata = ${Pereplata}`)
