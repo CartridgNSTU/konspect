cabd3cf67ae5ce398feb383bc0730668bec1f1e7
CartridgNSTU <97868606+CartridgNSTU@users.noreply.github.com>
2022-02-14 12:46:38 +0700

    кредит

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ let p = 10 ;
 let years = 5 ;
 //считаем дискриминант//
 let Pereplata = 2000000;
-if (years >0) {
-Pereplata = (Pereplata/100*10);
-years - 1;
-}
+
+Pereplata = (Pereplata/100*p)*years;
 
 console.log(`Pereplata = ${Pereplata}`)
